d90e011a6251e3261632ceba1e4b7f41590a3a6d
Marc ALCARAZ <ekameleon@gmail.com>
2016-11-23 18:08:47 +0100

    fix the toUnicodeNotation example

--- src/core/numbers/toUnicodeNotation.js
+++ src/core/numbers/toUnicodeNotation.js
@@ -2,11 +2,17 @@
 
 /**
  * Returns the unicode string notation of the specified numeric value.
- * @name aop
+ * @name toUnicodeNotation
  * @memberof core.numbers
  * @function
  * @param {number} num - The number to transform in a unicode string.
- * @return the unicode string notation of the specified numeric value.
+ * @return The unicode string notation of the specified numeric value.
+ * @example
+ * trace( toUnicodeNotation(  0) ) ; // "0000"
+ * trace( toUnicodeNotation( 10) ) ; // "000a"
+ * trace( toUnicodeNotation( 15) ) ; // "000f"
+ * trace( toUnicodeNotation( 16) ) ; // "0010"
+ * trace( toUnicodeNotation(255) ) ; // "00ff"
  */
 export function toUnicodeNotation( num )
 {
