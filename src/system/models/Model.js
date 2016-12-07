f87ccd92f96c80b7e89895f5e1d341e52aa4bc87
Benoit POUZET <bpouzet@gmail.com>
2016-12-07 16:33:45 +0100

    add lock to Model

--- src/system/models/Model.js
+++ src/system/models/Model.js
@@ -10,11 +10,45 @@ import { Lockable } from "../process/Lockable.js" ;
  * @extends system.process.Lockable
  * @extends system.data.Validator
  */
-export function Model() {}
+export function Model()
+{
+    Object.defineProperties( this ,
+    {
+        /**
+         * @private
+         */
+        __lock__ : { writable : true  , value : false }
+    }) ;
+}
 
 Model.prototype = Object.create( Lockable.prototype ,
 {
     constructor : { writable : true , value : Model } ,
+
+    /**
+     * Returns <code>true</code> if the object is locked.
+     * @return <code>true</code> if the object is locked.
+     * @name isLocked
+     * @memberof system.models.Model
+     * @function
+     * @instance
+     */
+    isLocked : { writable : true , value : function()
+    {
+        return this.__lock__ ;
+    }},
+
+    /**
+     * Locks the object.
+     * @name lock
+     * @memberof system.models.Model
+     * @function
+     * @instance
+     */
+    lock : { writable : true , value : function()
+    {
+        this.__lock__ = true ;
+    }},
 
     /**
      * Returns <code>true</code> if the specific value is valid.
@@ -28,7 +62,7 @@ Model.prototype = Object.create( Lockable.prototype ,
     supports : { writable : true , value : function( value )
     {
         return value === value ;
-    }} ,
+    }},
 
     /**
      * Returns the string representation of this instance.
@@ -41,6 +75,18 @@ Model.prototype = Object.create( Lockable.prototype ,
     toString : { writable : true , value : function()
     {
         return '[' + this.constructor.name + ']' ;
+    }},
+
+    /**
+     * Unlocks the object.
+     * @name unlock
+     * @memberof system.models.Model
+     * @function
+     * @instance
+     */
+    unlock : { writable : true , value : function()
+    {
+        this.__lock__ = false ;
     }},
 
     /**
